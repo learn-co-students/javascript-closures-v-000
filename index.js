b6edadefa677185fcb39937e179c7a8126f3bc14
AliceKB <alicekb01@gmail.com@gmail.com>
2017-02-15 23:33:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (!meal) {
+      console.log('My belly is empty. Woe is me.');
+    } else if (meal === 'Mini-Me') {
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+  return {
+    whatsForDinner,
+    digest
+  };
+}
+
+const whatsForDinner = fatBastard('Kobe beef');
+whatsforDinner();
