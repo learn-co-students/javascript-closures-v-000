0fd191188a582382ec8c70b7b7bd903bb3e85381
Scott Young <young.scottw@gmail.com>
2017-03-06 17:14:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (meal === 'Mini-Me') {
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+// Returning an object with references to the two functions.
+  return {
+    whatsForDinner,
+    digest
+  };
+}
+
+
+
+function raspyDoorGuy() {
+  const password = 'yarr';
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) {
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Gonna take more than that!");
+    }
+  }
+
+  return {
+    givePassword, bribe
+  };
+}
