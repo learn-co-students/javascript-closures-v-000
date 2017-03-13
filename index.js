af1986e5ab47eeb88e2ebfe5b613086272478cd6
lizdenhup <lizdenhup@gmail.com>
2017-03-13 14:00:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function(whatsForDinner) {
+    if (meal === 'Mini-Me') {
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+  return whatsForDinner;
+}
+
+function raspyDoorGuy() {
+  const password = 'yarr';
+ 
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+ 
+  return {
+    givePassword
+  };
+}
