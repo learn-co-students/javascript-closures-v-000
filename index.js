2da03362a65a6b51d80ff30ed2157a24934986c1
walwoodr <walwoodr@gmail.com>
2017-04-10 20:27:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function hat() {
+  const bestHat = 'cloche';
+
+  function wearIt(aHat) {
+    if (aHat === bestHat ) {
+      console.log('Good hat, put it on');
+    } else {
+      console.log('Bad hat, throw away');
+    }
+  };
+
+  function tellThem(complaint) {
+    return complaint === "I'm at my wit's end!";
+  }
+
+  function complain(theyComplain) {
+    if (tellThem(theyComplain)) {
+      return bestHat;
+    } else {
+      console.log("Nope")
+    }
+  }
+
+  return {
+    wearIt,
+    theyComplain
+  };
+}
