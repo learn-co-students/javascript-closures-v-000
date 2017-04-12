93c2a46addf196044e0d2a3d201dbfb92b4c8e9b
Bryan Hou <bryan.hou94@gmail.com>
2017-04-12 05:23:12 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,40 @@ function fatBastard(meal) {
   return {whatsForDinner, digest};
 }
 
-const { whatsForDinner, digest } = fatBastard('ribeye');
+const {whatsForDinner, digest} = fatBastard('ribeye');
 whatsForDinner();
 digest();
 whatsForDinner();
+
+
+function raspyDoorGuy() {
+  const password = 'yarr';
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) {
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Pssht. That won't work.");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  };
+}
+
+const { bribe } = raspyDoorGuy();
+bribe(80);
