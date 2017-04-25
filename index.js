fcc3883a2fa1351d6bbc2863b175283c75017b72
Brie Coyle <brie.coyle@gmail.com>
2017-04-24 20:29:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (meal === "Mini-me") {
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+  return whatsForDinner;
+}
+
+const whatsForDinner = fatbastard('Kobe beef');
+whatsForDinner();
