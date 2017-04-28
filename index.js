f66f6469eb77a4670d4616da8924a650720a67b2
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-28 14:19:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (meal === "Mini-Me") {
+      console.log("The wee man is in my belly!")
+    } else {
+      console.log(`I'm eatin a bit of ${meal}! Burp.`)
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+  return {
+    whatsForDinner,
+    digest
+  }
+}
