8c34c4fddff3ee9d676f8d9ca37165c6723334e7
Malind Kowalski <malind.nikulski@gmail.com>
2017-05-17 09:38:24 -0600

    completed js closure readme

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (meal === 'Mini-Me') {
+      console.log('The wee man is in my Belly!')
+    }
+    else {
+      console.log(`I'm eatin' a bit of ${meal}! BURP`)
+    }
+  }
+  return whatsForDinner
+}
