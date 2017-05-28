fd66292bd5a6fa0adfe6e5120653286e8d650594
Trai Compton <tlcompton2546@outlook.com>
2017-05-28 22:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-const app = "I don't do much."
+function fatBastard(meal) {
+  function whatsForDinner() {
+    if (!meal) {
+      console.log("My belly is empty. Woe is me.");
+    } else if (meal === 'Mini-Me') {
+      console.log("The wee man is in my belly!");
+    } else {
+      console.log(`I'm having a bit of ${meal}.`);
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+  return {
+    whatsForDinner,
+    digest
+  } // the functions are being returned as a reference to their name. They have not been called yet and can be set as variables to be called at a later time.
+}
+
+
+const whatsInHisTummy = fatBastard('Kobe Steak');
+whatsInHisTummy(); // Prints "I'm having a wee bit of Kobe Steak."
+
+const {
+  whatsForDinner,
+  digest
+} = fatBastard('ribeye');
+
+whatsForDinner(); // returns "I'm having a wee bit of ribeye."
+digest(); // sets meal to undefined
+whatsForDinner(); //returns "My Belly is empty. Woe is me." because the meal was set to undefined when the digest function was executed.
+
+
+function raspyDoorGuy() {
+  const password = "yarr"; //because it was defined within the function, the variable is not able to be accessed outside the function. However the inner function can access the variable.
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log("Ye may enter.");
+    } else {
+      console.log("Begone, landlubber !!");
+    }
+  }
+
+  // considered a 'private' function becaus eit is not being returned to be accessed outside of the function
+  function willBreakPrinciples(bribeAmount) {
+    return bribeAmount >= 50
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Pssht. What do you take me for?");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  } // only 2 functions are being returned to be accessed outside of the function
+}
+
+const { givePassword } = raspyDoorGuy();
+givePassword("kittens"); // returns "Begone,. landlubber !!"
+
+const { bribe } = raspyDoorGuy();
+bribe(80); // returns "yarr"
