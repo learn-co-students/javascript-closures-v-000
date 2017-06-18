cb9873a9f7e565479ef071273a3c571f6531e177
cquinones100 <cquinones100@gmail.com>
2017-06-18 12:05:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+
+  function givePassword(givenPassword) { // givePassword() is the inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount){
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Pssht. That won't work.")
+    }
+  }
+
+
+  return {
+    givePassword,
+    bribe
+  };
+}
