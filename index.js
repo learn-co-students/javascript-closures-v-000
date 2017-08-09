6f674c2fa5e9b7e14a972bf6ab1a89ba538cac15
Motti <=>
2017-08-09 15:43:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const app = "I don't do much."
- // 
+ 
+ // yesh li po function
+function fatBastard(meal) {
+  function whatsForDinner() { // whatsForDinner() is the inner function, a closure
+    if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function 
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+ // this return is for upper function
+  return whatsForDinner;
+}
