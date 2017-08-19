7de14556d2cbbfc2ef6c59cc915ce1da85100bb1
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-19 17:10:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+  function whatsForDinner(){
+
+
+    if (meal === "mini-me"){
+      console.log('the wee man is in my belly!')
+    } else {
+      console.log(`I'm eatin a bit of ${meal}! Burp.`)
+    }
+  }
+
+  return whatsForDinner
+}
