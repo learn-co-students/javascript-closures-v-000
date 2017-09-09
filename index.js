f06929d1ed53d8048a8e90aa1330ab70e0a92004
Apex-Code <upstaterounder@gmail.com>
2017-09-09 13:12:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+  function whatsForDinner(){
+    if (meal === "mini me"){
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log("I'm eatin' a bit of ${meal}!  Burp.");
+    }
+  }
+  return whatsForDinner;
+}
