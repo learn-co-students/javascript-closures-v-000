cf7f3720022a8fc004e5d1c7abff3d206f59dd66
ode-to-the-code <justcallmethis@gmail.com>
2017-09-15 16:31:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+
+function fatBastard(meal) {
+    function whatsForDinner() {
+      if (meal === "Mini-me") {
+        console.log('The wee man is in my belly!');
+      } else {
+        console.log(`I'm eating a wee bit of ${meal}! Burp.`)
+      }
+    }
+    return whatsForDinner;
+}
+
+
+const whatsForDinner = fatBastard('Kobe beef')
+whatsForDinner();
