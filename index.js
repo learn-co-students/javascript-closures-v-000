9a6509ae1ca38d3a481f6d26d5c7798e7e272a5c
Rob <github email address>
2018-02-21 06:29:18 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function fatBastard(meal) {
+  function whatsForDinner() {
+    //this is the inner function or Closure
+    if (!meal) {
+      console.log("My belly is empty. Woe is me.");
+    } else if (meal === "Mini-me") {
+      console.log("The wee man is in my belly!");
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  function digest() {
+    meal = undefined;
+  }
+
+  return {
+    whatsForDinner,
+    digest
+  };
+}
+
+const { whatsForDinner, digest } = fatBastard("ribeye");
+whatsForDinner();
+digest();
+whatsForDinner();
+
+function raspyDoorGuy() {
+  const password = "yarr";
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log("Ye may enter");
+    } else {
+      console.log("Begone, landlubber!");
+    }
+  }
+
+  return {
+    givePassword
+  };
+}
